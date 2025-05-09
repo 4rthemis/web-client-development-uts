0832fc07f09fa9e647acac5de2ce9b80f6399f42
4rthemis <syahrul.uix@gmail.com>
2025-05-09 22:33:49 +0700

    refactore(luasPersegi) : create export per function

--- tugas_1/javascript/luas-persegi.js
+++
@@ -1,5 +0,0 @@
-function luasPersegi(length, width) {
-  return length * width;
-}
-
-console.log(luasPersegi(3, 5));
---
+++ tugas_1/javascript/luasPersegi.js
@@ -0,0 +1,3 @@
+export function luasPersegi(length, width) {
+  return length * width;
+}
